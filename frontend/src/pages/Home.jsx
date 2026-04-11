04fdcace4e2f2d7ba83bc2bb2be3cf5f6d2259b4
enricorancaniusve <enrico.rancan@studenti.iusve.it>
2026-04-11 15:17:47 +0200

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react'
 import { api } from '../api/client'
 import ConfessionCard from '../components/ConfessionCard'
-import spioloImg from '../spiolo-main12.svg'
+import spioloImg from '../spiolo-main-def.svg'
 
 const CAT_DATA = [
   { id: null, name: 'Tutti', emoji: '🌐' },
